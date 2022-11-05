32c1ec74b665a891c3c68500f1b21fdb4a061ec2
Jeffrey Martinez <martinezjf2@gmail.com>
2022-11-05 16:46:25 -0400

    created package for the java class that was tried to import in udemy_practice

---
+++ com/practice/jsp/FunUtils/FunUtils.java
@@ -0,0 +1,10 @@
+package com.practice.jsp.FunUtils;
+
+public class FunUtils {
+    
+    public FunUtils() {}
+
+    public static String makeItLower(String data) {
+        return data.toLowerCase();
+    }
+}
---
+++ com/practice/jsp/FunUtils/Main.java
@@ -0,0 +1,7 @@
+package com.practice.jsp.FunUtils;
+
+public class Main {
+    public static void main(String[] args){
+     
+    }
+}
